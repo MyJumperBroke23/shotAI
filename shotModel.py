1c4915def674b5bcd434857a2ba60fe511bb21b0
MyJumperBroke23 <43692392+MyJumperBroke23@users.noreply.github.com>
2023-08-25 17:01:26 -0400

    Add model

---
+++ shotModel.py
@@ -0,0 +1,9 @@
+class ShotModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=25, hidden_size=50, num_layers=1, batch_first=True)
+        self.linear = nn.Linear(50, 1)
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        x = self.linear(x)
+        return (torch.sigmoid(x))[-1]
