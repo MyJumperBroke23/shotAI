fe031e41d45fbc64163fa3a66f832aff778bb1f9
MyJumperBroke23 <43692392+MyJumperBroke23@users.noreply.github.com>
2023-08-26 16:39:23 -0400

    update model

--- shotModel.py
+++ shotModel.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 class ShotModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.lstm = nn.LSTM(input_size=25, hidden_size=50, num_layers=1, batch_first=True)
+        self.lstm = nn.LSTM(input_size=75, hidden_size=50, num_layers=1, batch_first=True)
         self.linear = nn.Linear(50, 1)
     def forward(self, x):
         x, _ = self.lstm(x)
